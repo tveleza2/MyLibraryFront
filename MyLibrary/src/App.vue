51a6deee9354be015226819d607d1ded13d66ef8
tveleza2 <tveleza2@eafit.edu.co>
2025-03-06 21:28:25 -0500

    Project initialized, navbar component added

---
+++ MyLibrary/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+    <NavBar></NavBar>
+  </main>
+</template>
+
+
+
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
+
+
